58d21bfb677f169810625e1224df4a3aa9008c89
John Allers <john.allers@conductorone.com>
2025-06-27 12:39:17 -0400

    remove unused code

--- pkg/connector/custom_role.go
+++ pkg/connector/custom_role.go
@@ -30,25 +30,3 @@ func listGroupAssignedRoles(ctx context.Context, client *okta.Client, groupId st
 
 	return role, resp, nil
 }
-
-// listAssignedRolesForUser. List all user role assignments.
-// https://developer.okta.com/docs/api/openapi/okta-management/management/tag/RoleAssignmentAUser/#tag/RoleAssignmentAUser/operation/listAssignedRolesForUser
-func listAssignedRolesForUser(ctx context.Context, client *okta.Client, userId string) ([]*Roles, *okta.Response, error) {
-	apiPath, err := url.JoinPath(usersUrl, userId, "roles")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	reqUrl, err := url.Parse(apiPath)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var role []*Roles
-	resp, err := doRequest(ctx, reqUrl.String(), http.MethodGet, &role, client)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return role, resp, nil
-}
