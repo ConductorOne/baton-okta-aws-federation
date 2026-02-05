84bef94406d0e97fe3e2b43924d13168bc5c5ac4
Julián González <julian.gonzalez@conductorone.com>
2026-02-05 15:40:11 -0800

    [CE-247] Containerize baton-okta-aws-federation (#11)
    Most of the mechanical aspect of this pull request
    (the switch to ResourceSyncerV2, function arguments / return value changes)
    was done via Claude; the connector didn't need much notable work here.
    
    What is important to call out here is the removal of the manual
    appGroupCache / notAppGroupCache (the Okta client's internal cache remains)
    and settings mutex cache and their replacements with usage of the session store
    when possible.
    This was mostly straightforward, but sanity checks are welcome.
    
    Also, while we're here, fix the linter target by including
    the proper dependencies and the rules file.

--- pkg/config/conf.gen.go
+++ pkg/config/conf.gen.go
@@ -13,7 +13,7 @@ type OktaAwsFederation struct {
 	AwsAllowGroupToDirectAssignmentConversionForProvisioning bool `mapstructure:"aws-allow-group-to-direct-assignment-conversion-for-provisioning"`
 }
 
-func (c* OktaAwsFederation) findFieldByTag(tagValue string) (any, bool) {
+func (c *OktaAwsFederation) findFieldByTag(tagValue string) (any, bool) {
 	v := reflect.ValueOf(c).Elem() // Dereference pointer to struct
 	t := v.Type()
 
@@ -45,11 +45,13 @@ func (c *OktaAwsFederation) GetString(fieldName string) string {
 	if !ok {
 		return ""
 	}
-	t, ok := v.(string)
-	if !ok {
-		panic("wrong type")
+	if t, ok := v.(string); ok {
+		return t
 	}
-	return t
+	if t, ok := v.([]byte); ok {
+		return string(t)
+	}
+	panic("wrong type")
 }
 
 func (c *OktaAwsFederation) GetInt(fieldName string) int {
