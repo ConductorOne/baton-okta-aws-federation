92181ab502e12a7f9e076943a07e240e4ef83fb6
Julian Gonzalez <julian.gonzalez@conductorone.com>
2026-03-03 11:20:21 -0800

    Also check for resp.Response being nil

--- pkg/connector/aws_account.go
+++ pkg/connector/aws_account.go
@@ -710,7 +710,7 @@ func (o *accountResourceType) getOktaAppGroupFromCacheOrFetch(ctx context.Contex
 	if err != nil {
 		if resp == nil {
 			return nil, fmt.Errorf("okta-aws-connector: failed to fetch application group assignment: %w", err)
-		} else if resp.Body == nil {
+		} else if resp.Response == nil || resp.Body == nil {
 			// Treat as a possible 429 - Okta SDK can rarely construct a partial response.
 			return nil, handleOktaResponseErrorV5(ctx, resp, err)
 		}
